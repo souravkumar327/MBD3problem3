b7123603c1345f8e47a78c7af3e5c65a97413758
Sourav Kumar <souravkumar327@gmail.comgit config --global user.name 'souravkumar327>
2020-12-16 10:22:29 +0530

    changes were made in Graphs.m file

--- Graphs.m
+++ Graphs.m
@@ -1,11 +1,11 @@
-if ~exist('ABS_output','var')
-    disp('Did not find ABS_output dataset to plot results.');
-    disp('Please run simulation on the ABS model.');
+if ~exist('problem3_output','var')
+    disp('Did not find problem_output dataset to plot results.');
+    disp('Please run simulation on the problem3 model.');
 elseif isa(ABS_output,'Simulink.SimulationData.Dataset')
-    h = findobj(0, 'Name', 'ABS Speeds');
+    h = findobj(0, 'Name', 'problem3 Speeds');
     if isempty(h),
       h=figure('Position',[26   239   452   257],...
-               'Name','ABS Speeds',...
+               'Name','problem3 Speeds',...
                'NumberTitle','off');
     end
 
@@ -16,10 +16,10 @@ elseif isa(ABS_output,'Simulink.SimulationData.Dataset')
     % this prevents the main workspace from getting cluttered
 
     % plot wheel speed and car speed
-    plot(ABS_output.get('yout').Values.Vs.Time, ...
-         ABS_output.get('yout').Values.Vs.Data, ... 
-         ABS_output.get('yout').Values.Ww.Time, ...
-         ABS_output.get('yout').Values.Ww.Data);
+    plot(problem3_output.get('yout').Values.Vs.Time, ...
+         problem3_output.get('yout').Values.Vs.Data, ... 
+         problem3_output.get('yout').Values.speed.Time, ...
+         problem3_output.get('yout').Values.speed.Data);
     legend('Vehicle Speed \omega_v','Wheel Speed \omega_w','Location','best'); 
     title('Vehicle speed and wheel speed'); ylabel('Speed(rad/sec)'); xlabel('Time(sec)');
 
@@ -31,8 +31,8 @@ elseif isa(ABS_output,'Simulink.SimulationData.Dataset')
     end
 
     figure(h);
-    plot(ABS_output.get('slp').Values.Time, ...
-         ABS_output.get('slp').Values.Data);
+    plot(problem3_output.get('slp').Values.Time, ...
+         problem3_output.get('slp').Values.Data);
     title('Slip'); xlabel('Time(sec)'); ylabel('Normalized Relative Slip');
 
 end
